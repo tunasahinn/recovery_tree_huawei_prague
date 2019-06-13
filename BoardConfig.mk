f4552813e76f4e6aeb02f5f2414b59adf0d35245
Tunahan <49642331+tunasahinn@users.noreply.github.com>
2019-06-13 04:11:18 +0300

    Updated BoardConfig with needed stuffs.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -31,7 +31,6 @@ DEVICE_PATH := device/huawei/prague
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_ABI2 :=
 TARGET_CPU_VARIANT := generic
 
 TARGET_2ND_ARCH := arm
